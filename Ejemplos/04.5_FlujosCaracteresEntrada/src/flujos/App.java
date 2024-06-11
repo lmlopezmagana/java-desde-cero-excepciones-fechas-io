c2bbfdd9344c76eff72e52aafca045d4cd2cfacd
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-06-11 20:28:49 +0200

    Cambios finales

--- Ejemplos/04.5_FlujosCaracteresEntrada/src/flujos/App.java
+++ Ejemplos/04.5_FlujosCaracteresEntrada/src/flujos/App.java
@@ -1,28 +1,22 @@
 package flujos;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		List<String> quijoteLines = List.of(
-	            "En un lugar de la Mancha,",
-	            "de cuyo nombre no quiero acordarme,",
-	            "no ha mucho tiempo que vivía",
-	            "un hidalgo de los de lanza en astillero,",
-	            "adarga antigua, rocín flaco y galgo corredor."
-	        );
-		
-		
-		try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("quijote.txt")))) {
-			for(String s : quijoteLines) {
-				pw.println(s);
+		try (BufferedReader br = new BufferedReader(new FileReader("quijote.txt"))) {
+			String str;
+			while ((str = br.readLine()) != null) {
+				System.out.println(str);
 			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
